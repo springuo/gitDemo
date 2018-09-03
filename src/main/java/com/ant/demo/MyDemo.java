e6632cdfc71a47b78252fdbfdaff7915ece8a7a2
bj <bj@126.com>
2018-09-03 15:42:00 +0800

    IDEA整合Git测试

---
+++ src/main/java/com/ant/demo/MyDemo.java
@@ -0,0 +1,16 @@
+package com.ant.demo;
+
+/**
+ * ClassName:MyDemo
+ * Package:com.ant.demo
+ * Description:
+ *
+ * @Date 2018/9/3 15:37
+ * @Author guoxin
+ */
+public class MyDemo {
+
+    public static void main(String[] args) {
+        System.out.println("IDEA集成Git");
+    }
+}
